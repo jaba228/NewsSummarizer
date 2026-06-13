51f8d66f0dc607ab74425f4efbcbaaee08bcb89e
jaba228 <martin948173@gmail.com>
2026-06-13 12:39:45 +0300

    implemented IEquatable interface and equality methods in BaseEntity class, thinking of using record instead, need to do research here

--- src/NewsSummarizer.Domain/Common/BaseEntity.cs
+++ src/NewsSummarizer.Domain/Common/BaseEntity.cs
@@ -1,13 +1,58 @@
 namespace NewsSummarizer.Domain;
 
-abstract class BaseEntity
+// TODO: learned that i can maybe use record instead of
+//       abstract class, because it automatically
+//       implements all equality methods. Research
+//       on that topic needed.
+abstract class BaseEntity : IEquatable<BaseEntity>
 {
     // TODO: maybe use something like uuid for all 
     //       of the classes? 
-    public long Id;
+    public long Id { get; protected set; }
 
-    public BaseEntity(long Id)
+    protected BaseEntity() {}
+
+    protected BaseEntity(long id)
     {
-        this.Id = Id;
+        Id = id;
+    }
+
+    public bool Equals(BaseEntity? other)
+    {
+        if (other is null) return false;
+
+        if (ReferenceEquals(this, other)) return true;
+
+        if (GetType() != other.GetType()) return false;
+
+        if (Id == default || other.Id == default)
+            return false;
+        
+        return Id.Equals(other.Id);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return Equals(obj as BaseEntity);
+    }
+
+    public override int GetHashCode()
+    {
+        if (Id == 0) 
+            return base.GetHashCode();
+        
+        return HashCode.Combine(GetType(), Id); 
+    }
+
+    public static bool operator ==(BaseEntity? left, BaseEntity? right)
+    {
+        if (left is null) return right is null;
+
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(BaseEntity? left, BaseEntity? right)
+    {
+        return !(left == right);
     }
 }
